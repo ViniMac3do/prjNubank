5fec43f0897eb64f9e07f929a097ebc5a11b0538
vinimac3do <viniciu091@gmail.com>
2025-09-10 22:22:41 -0300

    Criacao da tela de login

--- Front-end/src/pages/login/style.js
+++ Front-end/src/pages/login/style.js
@@ -1,33 +1,110 @@
-// style.js
 import { StyleSheet } from "react-native";
-
+/* eslint-disable prettier/prettier */
 export default StyleSheet.create({
   container: {
-    flex: 1,                 // ocupa a tela toda
-    backgroundColor: "#8309d0", // roxo Nubank
-    justifyContent: "center", // centraliza no eixo Y
-    alignItems: "center",     // centraliza no eixo X
-    padding: 20,              // espaço interno
+    flex: 1,
+    backgroundColor: "#8309d0",
+    justifyContent: "center",
+    alignItems: "center",
   },
 
-  logo: {
-    width: 150,
-    height: 150,
-    marginBottom: 20,
+  background: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
   },
-  loading: {
-    marginTop: 20,
-    marginBottom: 20,
+
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    width: "100%",
+    paddingBottom: 60,
   },
-  titulo: {
-    fontSize: 24,
+
+  buttonPrimary: {
+    width: "90%",
+    backgroundColor: "#8309d0",
+    paddingVertical: 16,
+    borderRadius: 25,
+    alignItems: "center",
+    marginBottom: 12,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    elevation: 3,
+  },
+
+  buttonSecondary: {
+    width: "90%",
+    backgroundColor: "transparent",
+    borderWidth: 2,
+    borderColor: "#fff",
+    paddingVertical: 16,
+    borderRadius: 25,
+    alignItems: "center",
+    marginBottom: 12,
+  },
+
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
     fontWeight: "bold",
-    color: "#FFF",            // branco para contraste
+  },
+
+  buttonTextSecondary: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+
+  // Modal styles
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContent: {
+    width: "85%",
+    backgroundColor: "#fff",
+    borderRadius: 20,
+    padding: 24,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  input: {
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#8309d0",
+    borderRadius: 10,
+    padding: 12,
+    marginBottom: 16,
+    fontSize: 16,
+    backgroundColor: "#f7f7f7",
+  },
+  loginButton: {
+    width: "100%",
+    backgroundColor: "#8309d0",
+    paddingVertical: 14,
+    borderRadius: 10,
+    alignItems: "center",
     marginBottom: 10,
   },
-
-  subtitulo: {
+  loginButtonText: {
+    color: "#fff",
     fontSize: 16,
-    color: "#EEE",
+    fontWeight: "bold",
+  },
+  closeModalText: {
+    color: "#8309d0",
+    fontSize: 16,
+    marginTop: 8,
+    fontWeight: "bold",
   },
 });
